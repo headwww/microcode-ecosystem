4d0995686cc0e05f6a923ab85e3424fd56726065
shuwen <1243889238@qq.com>
2025-02-13 16:10:28 +0800

    build: 配置打包

--- packages/plugin-base-monaco-editor/src/index.ts
+++ packages/plugin-base-monaco-editor/src/index.ts
@@ -1,4 +1,7 @@
+import { SingleMonacoEditor } from './editor';
+
 export * from './editor';
 export * from './helper';
 export * from './monaco';
 export * from './controller';
+export default SingleMonacoEditor;
